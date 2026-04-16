a8220d9822f9917514a4ca09f8a9f5cbe98a5976
tvLors <lorsconsultingllc@gmail.com>
2026-04-16 15:12:36 -0500

    Weather update: Thu 04/16/2026 15:12:36.18

--- PushWeatherData2Github.bat
+++ PushWeatherData2Github.bat
@@ -25,9 +25,10 @@ if %ERRORLEVEL% EQU 0 (
     echo Changes detected, pushing to GitHub...
 
     REM If _BreezeOK.txt exists then it's OK to leave Back Porch Door open,
-                         so use Basic_BreezeOK.htm instead
+           so use Basic_BreezeOK.htm instead
     IF EXIST "_BreezeOK.txt" Copy /y BasicBreezeOK.htm Basic.htm
-    
+    timeout /t 10 /nobreak
+
     REM Push to GitHub
     git push origin main
     
