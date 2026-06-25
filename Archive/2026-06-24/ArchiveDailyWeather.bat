d8dd724c8ac7384f73e0571d444e0927bc7c5448
tvLors <lorsconsultingllc@gmail.com>
2026-06-25 00:02:29 -0500

    Weather update: Thu 06/25/2026  0:02:28.81

---
+++ Archive/2026-06-24/ArchiveDailyWeather.bat
@@ -0,0 +1,13 @@
+MD Archive\%DATE:~10,4%-%DATE:~4,2%-%DATE:~7,2%
+MD Archive\%DATE:~10,4%-%DATE:~4,2%-%DATE:~7,2%\Current
+MD Archive\%DATE:~10,4%-%DATE:~4,2%-%DATE:~7,2%\Daily
+MD Archive\%DATE:~10,4%-%DATE:~4,2%-%DATE:~7,2%\Weekly
+MD Archive\%DATE:~10,4%-%DATE:~4,2%-%DATE:~7,2%\Monthly
+MD Archive\%DATE:~10,4%-%DATE:~4,2%-%DATE:~7,2%\Yearly
+
+Copy D:\Users\Ginny\Documents\GitHub\Davis_Weather\*.* D:\Users\Ginny\Documents\GitHub\Davis_Weather\Archive\%DATE:~10,4%-%DATE:~4,2%-%DATE:~7,2%\*.*
+Copy D:\Users\Ginny\Documents\GitHub\Davis_Weather\Current\*.* D:\Users\Ginny\Documents\GitHub\Davis_Weather\Archive\%DATE:~10,4%-%DATE:~4,2%-%DATE:~7,2%\Current\*.*
+Copy D:\Users\Ginny\Documents\GitHub\Davis_Weather\Daily\*.* D:\Users\Ginny\Documents\GitHub\Davis_Weather\Archive\%DATE:~10,4%-%DATE:~4,2%-%DATE:~7,2%\Daily\*.*
+Copy D:\Users\Ginny\Documents\GitHub\Davis_Weather\Weekly\*.* D:\Users\Ginny\Documents\GitHub\Davis_Weather\Archive\%DATE:~10,4%-%DATE:~4,2%-%DATE:~7,2%\Weekly\*.*
+Copy D:\Users\Ginny\Documents\GitHub\Davis_Weather\Monthly\*.* D:\Users\Ginny\Documents\GitHub\Davis_Weather\Archive\%DATE:~10,4%-%DATE:~4,2%-%DATE:~7,2%\Monthly\*.*
+Copy D:\Users\Ginny\Documents\GitHub\Davis_Weather\Yearly\*.* D:\Users\Ginny\Documents\GitHub\Davis_Weather\Archive\%DATE:~10,4%-%DATE:~4,2%-%DATE:~7,2%\Yearly\*.*
